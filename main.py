a101d7f56b82c89bee1e9bf7a49273197041be22
Ahmed Ali <theahmedjoyali@gmail.com>
2026-04-26 12:05:03 -0700

    Successfully created a local Inngest Client testbed with FastAPI

--- main.py
+++ main.py
@@ -1,6 +1,29 @@
-def main():
-    print("Hello from rag-pdf-scanner!")
+import logging
+from fastapi import FastAPI
+import inngest
+import inngest.fast_api
+from inngest.experimental import ai
+from dotenv import load_dotenv
+import uuid
+import os
+import datetime
 
+load_dotenv()
 
-if __name__ == "__main__":
-    main()
+inngest_client = inngest.Inngest(
+    app_id = "rag_pdf_scanner",
+    logger = logging.getLogger("uvicorn"),
+    is_production = False,
+    serializer = inngest.PydanticSerializer()
+)
+
+@inngest_client.create_function(
+    fn_id = "RAG: Inngest PDF",
+    trigger = inngest.TriggerEvent(event="rag/inngest_pdf")
+)
+async def rag_inngest_pdf(ctx: inngest.Context):
+    return {"hello": "world"}
+
+app = FastAPI()
+
+inngest.fast_api.serve(app, inngest_client, [rag_inngest_pdf])
